ed8b17f9cd7a1d6ec709c052319743e1d299dca7
busyStone <jlu.xzp@gmail.com>
2016-04-07 15:19:30 +0800

    Humming V1.0.2 -> SkyBorne:Copter V1.0.2.dev

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "Humming V1.0.2"
+#define THISFIRMWARE "SkyBorne:Copter V1.0.2.dev"
 #define FIRMWARE_VERSION 3,3,2,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 /*
