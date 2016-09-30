bcde7bf2506ed414d88c79a23565a6d55d0aa40b
busyStone <jlu.xzp@gmail.com>
2016-09-30 19:51:24 +0800

    version -> v1.0.2.8

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,7 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "SkyBorne:Copter v1.0.2.beta.7"
-#define FIRMWARE_VERSION 1,0,2,((uint8_t)FIRMWARE_VERSION_TYPE_BETA + (uint8_t)7)
+#define THISFIRMWARE "SkyBorne:Copter v1.0.2.beta.8"
+#define FIRMWARE_VERSION 1,0,2,FIRMWARE_VERSION_TYPE_BETA + 8
 
 /*
    This program is free software: you can redistribute it and/or modify
